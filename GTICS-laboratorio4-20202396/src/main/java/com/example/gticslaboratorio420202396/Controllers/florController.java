06ff25cc3dfaeae66d299f803e0bbd9a45c15ec3
dodo240703 <a20202396@pucp.edu.pe>
2024-09-19 21:37:50 -0500

    segundo avance
    A

--- GTICS-laboratorio4-20202396/src/main/java/com/example/gticslaboratorio420202396/Controllers/florController.java
+++ GTICS-laboratorio4-20202396/src/main/java/com/example/gticslaboratorio420202396/Controllers/florController.java
@@ -1,15 +1,39 @@
 package com.example.gticslaboratorio420202396.Controllers;
 
+import com.example.gticslaboratorio420202396.Models.Entities.Flor;
+import com.example.gticslaboratorio420202396.Models.Repositories.FlorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/flores")
 public class florController {
 
+    final FlorRepository florRepository;
+
+    public florController(FlorRepository florRepository) {
+        this.florRepository = florRepository;
+    }
+
     @GetMapping("/Catalogo")
-    public String showCatalogo() {
+    public String showCatalogo(@RequestParam(required = false) String color,
+                               @RequestParam(required = false) String tipo,
+                               @RequestParam(required = false) String ocasion,
+                               Model model) {
+        List<Flor> listaFlowers;
+        // Si se aplican filtros, busca con base en esos filtros
+        if (color != null && tipo != null && ocasion != null) {
+            listaFlowers = florRepository.findByColor_NombreColorAndTipo_NombreTipoAndOcasion_NombreOcasion(color, tipo, ocasion);
+        } else {
+            listaFlowers = florRepository.findAll();
+        }
+        model.addAttribute("listaFlowers", listaFlowers);
+        model.addAttribute("totalFlores", listaFlowers.size());
         return "catalogoPage";
     }
     @GetMapping("/Entretenimiento")
