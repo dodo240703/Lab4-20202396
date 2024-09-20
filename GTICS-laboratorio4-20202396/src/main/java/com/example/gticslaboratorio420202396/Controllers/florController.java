e2bc73c426b30b5c3bc258ea4a9730a7d5f643ec
dodo240703 <a20202396@pucp.edu.pe>
2024-09-19 23:27:52 -0500

    Avance 3
    Aún no lo subo a la nube pipipi

--- GTICS-laboratorio4-20202396/src/main/java/com/example/gticslaboratorio420202396/Controllers/florController.java
+++ GTICS-laboratorio4-20202396/src/main/java/com/example/gticslaboratorio420202396/Controllers/florController.java
@@ -1,10 +1,14 @@
 package com.example.gticslaboratorio420202396.Controllers;
 
 import com.example.gticslaboratorio420202396.Models.Entities.Flor;
+import com.example.gticslaboratorio420202396.Models.Entities.Usuario;
 import com.example.gticslaboratorio420202396.Models.Repositories.FlorRepository;
+import com.example.gticslaboratorio420202396.Models.Repositories.UserRepository;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,9 +21,11 @@ import java.util.List;
 public class florController {
 
     final FlorRepository florRepository;
+    final UserRepository userRepository;
 
-    public florController(FlorRepository florRepository) {
+    public florController(FlorRepository florRepository, UserRepository userRepository) {
         this.florRepository = florRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/Catalogo")
@@ -41,16 +47,27 @@ public class florController {
     @GetMapping("/Entretenimiento")
     public String showEntretenimiento(Model model) {
         List<Flor> listaFlowers = florRepository.findAll();
+        List<Usuario> listaUsuarios = userRepository.findAllByOrderByPuntuacionDesc();
+
 
         // Duplica y mezcla las flores para tener pares
         List<Flor> cartas = new ArrayList<>(listaFlowers);
         cartas.addAll(listaFlowers); // Duplicar para los pares
         Collections.shuffle(cartas); // Mezclar las cartas
 
-
+        model.addAttribute("listaUsuarios", listaUsuarios);
         model.addAttribute("listaFlowers", cartas);
         return "entretenimientoPage";
     }
+    @PostMapping("/Entretenimiento")
+    public String submitUserScore(Model model, Usuario usuario) {
+        // Guardar el usuario con el nickname y puntuación
+        userRepository.save(usuario);
+
+        // Redirigir a la misma página para mostrar la lista actualizada
+        return "redirect:/flores/Entretenimiento";
+    }
+
     @GetMapping("/Carrito")
     public String showCarrito() {
         return "carritoPage";
